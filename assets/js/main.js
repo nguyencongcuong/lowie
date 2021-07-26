4c69e53a4aab030527486ba11e0f701a2356ab0f
Nguyễn Công Cương <cuongnc.min@gmail.com>
2021-07-26 22:42:12 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,13 @@ const sounds = [
     {
         id: "001",
         name: "forest",
-        audioURL: "./assets/sounds/rain.mp3",
+        audioURL: "./assets/sounds/forest.mp3",
         imageURL: "./assets/images/forest.png"
     },
     {
         id: "002",
         name: "macaw",
-        audioURL: "./assets/sounds/rain.mp3",
+        audioURL: "./assets/sounds/macaw.mp3",
         imageURL: "./assets/images/macaw.png"
     },
     {
@@ -21,56 +21,67 @@ const sounds = [
     {
         id: "004",
         name: "river",
-        audioURL: "./assets/sounds/rain.mp3",
+        audioURL: "./assets/sounds/river.mp3",
         imageURL: "./assets/images/river.png"
     },
     {
         id: "005",
         name: "sea",
-        audioURL: "./assets/sounds/rain.mp3",
+        audioURL: "./assets/sounds/sea.mp3",
         imageURL: "./assets/images/sea.png"
     },
     {
         id: "006",
         name: "water",
-        audioURL: "./assets/sounds/rain.mp3",
+        audioURL: "./assets/sounds/water.mp3",
         imageURL: "./assets/images/water.png"
     },
     {
         id: "007",
         name: "waterfall",
-        audioURL: "./assets/sounds/rain.mp3",
+        audioURL: "./assets/sounds/waterfall.mp3",
         imageURL: "./assets/images/waterfall.png"
     },
     {
         id: "008",
         name: "wind",
-        audioURL: "./assets/sounds/rain.mp3",
+        audioURL: "./assets/sounds/wind.mp3",
         imageURL: "./assets/images/wind.png"
     },
     {
         id: "009",
         name: "campfire",
-        audioURL: "./assets/sounds/rain.mp3",
+        audioURL: "./assets/sounds/campfire.mp3",
         imageURL: "./assets/images/campfire.png"
+    }
+]
+const backgroundAudio = [
+    {
+        id: "001",
+        name: "meditation_music_03",
+        audioURL: "./assets/background-audios/meditation_music_03.mp3",
+        imageURL: "./assets/images/lotus.png"
     }
 ]
 
 // Process
 createSoundCards("soundCards");
-sounds.map(element => addSound(element.id, "soundCards"));
+sounds.map(element => addSound(element.id, "soundCards", sounds));
+createSoundCards("backgroundAudioCards");
+backgroundAudio.map(element => addSound(element.id, "backgroundAudioCards", backgroundAudio));
 
 // Functions
 function createSoundCards(id) {
     const main = document.getElementById("main");
-    main.innerHTML = `<section id=${id} class="sound-cards"></section>`;
+    const item = `<section id=${id} class="sound-cards"></section>`
+    main.insertAdjacentHTML('beforeend', item);
 }
 
-function addSound(id, parent) {
+function addSound(id, parent, library) {
 
     // Variables
     let soundCards = document.getElementById(parent);
-    let sound = sounds.filter(a => a.id == id);
+    let sound = library.filter(a => a.id == id);
     let soundURL = sound.reduce((a, b) => b.audioURL, 0);
     let imageURL = sound.reduce((a, b) => b.imageURL, 0);
     let soundName = sound.reduce((a, b) => b.name, 0);
@@ -90,9 +101,11 @@ function addSound(id, parent) {
     // Clicks to play or pause
 
     soundItem.addEventListener("click", function () {
+        
         soundItem.classList.toggle("sound--disabled");
         soundItem.classList.toggle("sound--enabled");
-        if (soundItem.className == "sound--enabled") {
+        
+        if (soundItem.className == "sound sound--enabled") {
 
             if (typeof audio.loop == 'boolean') {
                 audio.loop = true;
